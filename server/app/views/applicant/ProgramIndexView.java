9270b393b78d67faf71d9b0d0485ed8c40f169b7
Austin Lee <avaleske@users.noreply.github.com>
2024-09-16 22:58:31 +0000

    spike on htmx for categories

--- server/app/views/applicant/ProgramIndexView.java
+++ server/app/views/applicant/ProgramIndexView.java
@@ -571,10 +571,10 @@ public final class ProgramIndexView extends BaseHtmlView {
       Messages messages) {
     return form()
         .withId("category-filter-form")
-        .withAction(
-            controllers.applicant.routes.ApplicantProgramsController.indexWithApplicantId(
-                    applicantId, ImmutableList.of())
-                .url())
+        // .withAction(
+        //     controllers.applicant.routes.ApplicantProgramsController.indexWithApplicantId(
+        //             applicantId, ImmutableList.of())
+        //         .url())
         .withMethod("GET")
         .with(
             fieldset(
@@ -587,6 +587,14 @@ public final class ProgramIndexView extends BaseHtmlView {
                                 .withId("filter-chip")
                                 .with(
                                     input()
+                                        .attr("hx-get", controllers.applicant.routes.ApplicantProgramsController.indexWithApplicantId(
+                                            applicantId, ImmutableList.of())
+                                        .url())
+                                        .attr("hx-include", "[id*=\"check-category\"]")
+                                        .attr("hx-params", "categories")
+                                        .attr("hx-target", "body")
+                                        .attr("hx-swap", "innerHTML, focus-scroll:true")
+                                        // .attr("hx-swap", )
                                         .withId("check-category-" + category)
                                         .withType("checkbox")
                                         .withName("categories")
